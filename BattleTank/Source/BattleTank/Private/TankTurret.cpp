8a2bb84151b1c9ccb7643df9d4fe1ab72f955b0a
rhinds1004 <rhinds1004@gmail.com>
2019-06-21 18:03:38 -0700

    created tank turret class

---
+++ BattleTank/Source/BattleTank/Private/TankTurret.cpp
@@ -0,0 +1,7 @@
+// Robert Hinds
+
+#include "TankTurret.h"
+
+
+
+
---
+++ BattleTank/Source/BattleTank/Public/TankTurret.h
@@ -0,0 +1,20 @@
+// Robert Hinds
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/StaticMeshComponent.h"
+#include "TankTurret.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_API UTankTurret : public UStaticMeshComponent
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
