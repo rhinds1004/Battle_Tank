4cb5d5aee90a100f4a1c5a797c7b03d54d44846b
rhinds1004 <rhinds1004@gmail.com>
2019-07-01 18:35:05 -0700

    Added tank movement coponent for fly by wire system

---
+++ BattleTank/Source/BattleTank/Public/TankMovementComponent.h
@@ -0,0 +1,20 @@
+// Robert Hinds
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/NavMovementComponent.h"
+#include "TankMovementComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_API UTankMovementComponent : public UNavMovementComponent
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
