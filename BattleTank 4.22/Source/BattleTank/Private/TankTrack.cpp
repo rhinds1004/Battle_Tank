45c1fb81654ada01bbb4a12fa0ecf663128f14c2
hinds <hinds@DESKTOP-T31IANB>
2019-07-25 18:59:19 -0700

    Added a function that calculates the force being applied from the size and applies an opposing force to counter act. It is intended to prevent the tank from sliding. Kinda of works but super flakey and take easily flys off into space due to the torque being applied by how we are applying the moving force to the tank. Also adjusted soem of the tank pyhsics attributes to help with the bad physics reactins.  Also added back in the tank pyysic materials as it was unassinted when rebuilding the tank class blueprint objects.

--- BattleTank 4.22/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank 4.22/Source/BattleTank/Private/TankTrack.cpp
@@ -13,6 +13,16 @@ void UTankTrack::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompo
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
+
+	//calculate the slippage speed
+	float SlippageSpeed = FVector::DotProduct(GetComponentVelocity(), GetRightVector());
+	//work-out the required acceleration this frame to correct
+	FVector CorrectionAcceleration = -SlippageSpeed / DeltaTime * GetRightVector();
+	//Calculate and apply sideways for (F = m * a)
+	UStaticMeshComponent* TankRoot = Cast<UStaticMeshComponent>(GetOwner()->GetRootComponent());
+	FVector CorrectionForce = (TankRoot->GetMass() * CorrectionAcceleration) / 2; //divide by two as there are two tracks.
+	//TankRoot->AddForce(CorrectionForce);																		 									  
+	TankRoot->AddForceAtLocation(CorrectionForce, GetComponentLocation());
 }
 
 void UTankTrack::SetThrottle(float ThrottleAmt)
