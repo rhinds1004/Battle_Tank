0b786f1611492d72893609fdda64c3786729f3b6
hinds <hinds@DESKTOP-T31IANB>
2019-07-28 15:14:50 -0700

    removed a todo from turret. as the bug where turret would freak out when move over 180 or -180 was fixed in the tan aiming component by normalizing the rotator.  per the unreal docs GetNormalized: "Create a copy of this rotator and normalize, removes all winding and creates the "shortest route" rotation."

--- BattleTank 4.22/Source/BattleTank/Private/TankTurret.cpp
+++ BattleTank 4.22/Source/BattleTank/Private/TankTurret.cpp
@@ -2,7 +2,7 @@
 
 #include "TankTurret.h"
 
-//TODO fix the fact it rotates all away around when it hits -180 degrees or 180. Do to how the orientation rotation calculation is done in tankaimingcomponent.
+
 void UTankTurret::RotateTurret(float RelativeSpeed)
 {
 	RelativeSpeed = FMath::Clamp<float>(RelativeSpeed, -1, 1);
