0ff8ff8810a4e12411b24927be85bb16ae380e1b
hinds <hinds@DESKTOP-T31IANB>
2019-08-11 13:48:39 -0700

    Added ImpactBlast particle emitter to projectile class. Added OnHit function that turns off LaunchBlast emitter and Mesh visibilty on projectile colliding with something and turns on impactblast emitter.

--- BattleTank 4.22/Source/BattleTank/Private/Projectile.cpp
+++ BattleTank 4.22/Source/BattleTank/Private/Projectile.cpp
@@ -10,7 +10,7 @@
 AProjectile::AProjectile()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
 	CollisionMesh = CreateDefaultSubobject<UStaticMeshComponent>(FName("Collision Mesh"));
 	SetRootComponent(CollisionMesh);
@@ -18,7 +18,14 @@ AProjectile::AProjectile()
 	CollisionMesh->SetVisibility(true);
 
 	LaunchBlast = CreateDefaultSubobject <UParticleSystemComponent>(FName("Launch Blast"));
-	LaunchBlast->AttachTo(CollisionMesh);
+	LaunchBlast->AttachToComponent(CollisionMesh, FAttachmentTransformRules::KeepRelativeTransform);
+	
+	ImpactBlast = CreateDefaultSubobject <UParticleSystemComponent>(FName("Impact Blast"));
+	ImpactBlast->AttachToComponent(CollisionMesh, FAttachmentTransformRules::KeepRelativeTransform);
+	ImpactBlast->bAutoActivate = false;
+
+
+
 	ProjectileMovementComponent = CreateDefaultSubobject<UProjectileMovementComponent>(FName("Projectile Movement"));
 	ProjectileMovementComponent->bAutoActivate = false;
 	InitialLifeSpan = 7.f;
@@ -28,6 +35,14 @@ AProjectile::AProjectile()
 void AProjectile::BeginPlay()
 {
 	Super::BeginPlay();	
+	CollisionMesh->OnComponentHit.AddDynamic(this, &AProjectile::OnHit);
+}
+
+void AProjectile::OnHit(UPrimitiveComponent * HitComponent, AActor * OtherActor, UPrimitiveComponent * OtherComponent, FVector NormalImpulse, const FHitResult & Hit)
+{
+	LaunchBlast->Deactivate();
+	ImpactBlast->Activate();
+	CollisionMesh->SetVisibility(false);
 }
 
 // Called every frame
--- BattleTank 4.22/Source/BattleTank/Public/Projectile.h
+++ BattleTank 4.22/Source/BattleTank/Public/Projectile.h
@@ -23,19 +23,28 @@ protected:
 
 	class UProjectileMovementComponent* ProjectileMovementComponent = nullptr;
 
-
-
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
-	void LaunchProjectile(float Speed);
-
 	UPROPERTY(VisibleAnywhere, Category = "Setup")
 		class UStaticMeshComponent* CollisionMesh = nullptr;
 
 	UPROPERTY(VisibleAnywhere, Category = "Setup")
 		class UParticleSystemComponent* LaunchBlast = nullptr;
 
+	UPROPERTY(VisibleAnywhere, Category = "Setup")
+		class UParticleSystemComponent* ImpactBlast = nullptr;
 	
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	void LaunchProjectile(float Speed);
+
+
+
+private:
+	// Function that is called when the projectile hits something.
+	UFUNCTION()
+		void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit);
+
+	
+
 };
